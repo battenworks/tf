ead36e07269504e8e67f3238278c01225a89c881
battenworks <djb@battenworks.com>
2022-02-01 23:23:46 -0700

    test

--- .github/build.sh
+++ .github/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux
 
-cd $BINARY_DIRECTORY
+cd ../$BINARY_DIRECTORY
 NAME="${BINARY_NAME}_${VERSION}_${GOOS}_${GOARCH}"
 
 EXT=''
@@ -12,5 +12,5 @@ fi
 
 ls -a
 
-tar cvfz ${NAME}.tar.gz "${BINARY_NAME}${EXT}"
-md5sum ${NAME}.tar.gz | cut -d ' ' -f 1 > ${NAME}_checksum.txt
+# tar cvfz ${NAME}.tar.gz "${BINARY_NAME}${EXT}"
+# md5sum ${NAME}.tar.gz | cut -d ' ' -f 1 > ${NAME}_checksum.txt
