415038a9c8563929a667c67b8520f489997fcc67
battenworks <djb@battenworks.com>
2022-02-01 22:55:50 -0700

    test

--- .github/build.sh
+++ .github/build.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -eux
 
-BINARY_NAME=$1
-NAME="${BINARY_NAME}_${VERSION}_${GOOS}_${GOARCH}"
+# PROJECT_NAME="$(basename $GITHUB_REPOSITORY)"
+# NAME="${PROJECT_NAME}_${VERSION}_${GOOS}_${GOARCH}"
 
-EXT=''
+# EXT=''
 
-if [ $GOOS == 'windows' ]; then
-  EXT='.exe'
-fi
+# if [ $GOOS == 'windows' ]; then
+#   EXT='.exe'
+# fi
 
-tar cvfz ${NAME}.tar.gz "${BINARY_NAME}${EXT}" LICENSE
-md5sum ${NAME}.tar.gz | cut -d ' ' -f 1 > ${NAME}_checksum.txt
+# tar cvfz ${NAME}.tar.gz "${PROJECT_NAME}${EXT}" LICENSE
+# md5sum ${NAME}.tar.gz | cut -d ' ' -f 1 > ${NAME}_checksum.txt
+
+cd ../artifacts
+ls -a
