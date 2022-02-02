e6766026a8d0926182511bbda637d66078e3fb31
battenworks <djb@battenworks.com>
2022-02-01 23:27:21 -0700

    test

--- .github/build.sh
+++ .github/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -eux
 
-cd ../$BINARY_DIRECTORY
+cd $BINARY_DIRECTORY
+ls -a
+
 NAME="${BINARY_NAME}_${VERSION}_${GOOS}_${GOARCH}"
 
 EXT=''
@@ -9,8 +11,6 @@ EXT=''
 if [ $GOOS == 'windows' ]; then
   EXT='.exe'
 fi
-
-ls -a
 
 # tar cvfz ${NAME}.tar.gz "${BINARY_NAME}${EXT}"
 # md5sum ${NAME}.tar.gz | cut -d ' ' -f 1 > ${NAME}_checksum.txt
